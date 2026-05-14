026ec3909d1a34b799d512d3f06861aac78d61a8
Christian Moser <christian@cmoser.eu>
2026-05-15 01:40:41 +0200

    fixed loading of django-browser-reload

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
+        path('__reload__/', include('django_browser_reload.urls')),
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
         *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     ]
